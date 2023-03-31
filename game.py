fb389497d629e84d874dbe2ce8cda22a16ebe49f
Alyovano <alyovaski@gmail.com>
2023-03-31 21:48:13 +0200

    SFVJKDGJD

---
+++ game.py
@@ -0,0 +1,13 @@
+class Game:
+    def __init__(self, screen):
+        self.screen = screen
+        
+    def set_up(self):
+        print("SET UP TEST")
+        
+    def update(self):
+        index = 0
+        while True:
+            print("valeur de i ", index)
+            index += 1
+            # print("UPDATE TEST")
