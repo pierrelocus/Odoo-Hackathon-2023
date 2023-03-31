2bd80fc9dad22475a24af90c6254b715437a1ade
Pierre Locus <lop@nalios.be>
2023-03-31 22:34:27 +0200

    [FIX] Limit to borders

--- player.py
+++ player.py
@@ -10,8 +10,13 @@ class Player:
         print("player updated")
 
     def update_position(self, x_change, y_change):
-        self.position[0] += x_change
-        self.position[1] += y_change
+        print('Positions : %s %s' % (self.position[0], self.position[1]))
+        if not (self.position[0] == 0 and x_change < 0) and \
+            not (self.position[0] * config.SCALE >= 590 and x_change > 0):
+            self.position[0] += x_change
+        if not (self.position[1] == 0 and y_change < 0) and \
+            not (self.position[1] * config.SCALE >= 390 and y_change > 0):
+            self.position[1] += y_change
 
     def render(self, screen):
         pygame.draw.rect(screen, config.WHITE, (self.position[0] * config.SCALE, self.position[1] * config.SCALE, config.SCALE, config.SCALE), 4)
