1074bd5b3a468de730b0865deb6eb4db6df60d92
sla <sla@niboo.com>
2023-04-01 14:35:41 +0200

    CHANGE ON TSX FILES

--- assets/fence_tiles.tsx
+++ assets/fence_tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.0" name="fence_tiles" tilewidth="32" tileheight="32" tilecount="16" columns="4">
- <image source="../../../Downloads/basic_tileset_and_assets_standard/fence_tiles.png" width="128" height="128"/>
+ <image source="fence_tiles.png" width="128" height="128"/>
 </tileset>
