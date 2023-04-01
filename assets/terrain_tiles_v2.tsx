db2ba9839777f43ccb3400d3934ad61102eba6c3
sla <sla@niboo.com>
2023-04-01 14:39:21 +0200

    CHANGE ON TSX FILES

--- assets/terrain_tiles_v2.tsx
+++ assets/terrain_tiles_v2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.0" name="terrain_tiles_v2" tilewidth="32" tileheight="32" tilecount="160" columns="10">
- <image source="terrain_tiles_v2.png" width="320" height="512"/>
+ <image source="../terrain_tiles_v2.png" width="320" height="512"/>
 </tileset>
--- assets/tiles.tsx
+++ assets/tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.0" name="tiles" tilewidth="32" tileheight="32" tilecount="144" columns="12">
- <image source="assets_spritesheet_v2_free.png" width="384" height="384"/>
+ <image source="../assets_spritesheet_v2_free.png" width="384" height="384"/>
 </tileset>
