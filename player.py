0894be0cfd768e92bd58ec278ed0e71dba659e7e
sla <sla@niboo.com>
2023-04-01 02:08:15 +0200

    acac

--- player.py
+++ player.py
@@ -1,21 +1,53 @@
 import pygame
-import config
 
-class Player:
-    def __init__(self, x_postition, y_position):
-        print("player created")
-        self.position = [x_postition, y_position]
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self, x, y):
+        super().__init__()
+        self.sprite_sheet = pygame.image.load("player.png")
+        self.image = self.get_image(0, 0)
+        self.image.set_colorkey([0, 0, 0])
+        self.rect = self.image.get_rect()
+        self.position = [x, y]
+        self.images = {
+            "up": self.get_image(0, 96),
+            "down": self.get_image(0, 0),
+            "right": self.get_image(0, 64),
+            "left": self.get_image(0, 32)
+        }
+        self.feet = pygame.Rect(0, 0, self.rect.width * 0.5, 12)
+        self.old_position = self.position.copy()
+        self.speed = 3
+
+    def get(self):
+        self.image = self.images["down"]
+        self.image.set_colorkey([0, 0, 0])
+        return self.image
+
+    def save_location(self): self.old_position = self.position.copy()
+
+    def move_player(self, type):
+        self.image = self.images[type]
+        self.image.set_colorkey([0, 0, 0])
+        if type == "up":
+            self.position[1] -= self.speed
+        elif type == "down":
+            self.position[1] += self.speed
+        elif type == "right":
+            self.position[0] += self.speed
+        elif type == "left":
+            self.position[0] -= self.speed
 
     def update(self):
-        print("player updated")
+        self.rect.topleft = self.position
+        self.feet.midbottom = self.rect.midbottom
 
-    def update_position(self, x_change, y_change):
-        if not (self.position[0] == 0 and x_change < 0) and \
-            not (self.position[0] * config.SCALE >= (config.WIDTH - config.SCALE) and x_change > 0):
-            self.position[0] += x_change
-        if not (self.position[1] == 0 and y_change < 0) and \
-            not (self.position[1] * config.SCALE >= (config.HEIGHT - config.SCALE) and y_change > 0):
-            self.position[1] += y_change
+    def move_back(self):
+        self.position = self.old_position
+        self.update()
 
-    def render(self, screen):
-        pygame.draw.rect(screen, config.WHITE, (self.position[0] * config.SCALE, self.position[1] * config.SCALE, config.SCALE, config.SCALE), 4)
+    def get_image(self, x, y):
+        image = pygame.Surface([32, 32])
+        image.blit(self.sprite_sheet, (0, 0), (x, y, 32, 32))
+        return image
